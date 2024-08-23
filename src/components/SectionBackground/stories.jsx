6392fa817b19c3ba0670668b3d72bb4e15d89c98
Alysson Zanchi <alyssonzanchi@gmail.com>
2024-08-23 18:09:24 -0300

    aula 138

---
+++ src/components/SectionBackground/stories.jsx
@@ -0,0 +1,30 @@
+import { SectionBackground } from '.';
+
+export default {
+  title: 'SectionBackground',
+  component: SectionBackground,
+  args: {
+    children: (
+      <div>
+        <h1>SectionBackground</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          nostrum? Deserunt similique adipisci quod architecto delectus.
+          Cupiditate deserunt amet inventore repellendus quas sint facilis
+          eaque, veritatis voluptate eos, pariatur incidunt!
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionBackground {...args} />
+    </div>
+  );
+};
---
+++ src/components/SectionContainer/stories.jsx
@@ -0,0 +1,30 @@
+import { SectionContainer } from '.';
+
+export default {
+  title: 'SectionContainer',
+  component: SectionContainer,
+  args: {
+    children: (
+      <div>
+        <h1>SectionContainer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni
+          sed, quia odit minima, quos aspernatur voluptatibus ullam vitae
+          aperiam, quas necessitatibus qui eius reiciendis deserunt dignissimos!
+          Atque, nam modi!
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionContainer {...args} />
+    </div>
+  );
+};
