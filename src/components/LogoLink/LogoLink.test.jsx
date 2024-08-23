80811bb9d63cc7d3c42438efe6b44b5ea86edb91
Alysson Zanchi <alyssonzanchi@gmail.com>
2024-08-23 17:11:04 -0300

    aula 136

---
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react';
+import { LogoLink } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<LogoLink />', () => {
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Olá mundo" />);
+    const heading = screen.getByRole('heading', { name: 'Olá mundo' });
+    expect(heading.firstChild).toHaveAttribute('href', '#target');
+  });
+
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Olá mundo" srcImg="image.jpg" />,
+    );
+    expect(screen.getByRole('img', { name: 'Olá mundo' })).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+});
