10b4f4dae3e43f7d62aebcee1bbea8dd4d3c5ffd
Alysson Zanchi <alyssonzanchi@gmail.com>
2024-08-27 16:29:04 -0300

    aula 144

---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Grid Two Columns',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Reprehenderit maxime qui quidem reiciendis dolorum dolorem nemo aperiam eos 
+    voluptates rem doloremque nobis quisquam a, voluptate officia facere? 
+    Rerum, saepe blanditiis!`,
+  srcImg: 'assets/images/javascript.svg',
+};
