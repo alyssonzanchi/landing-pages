d2fcf36a09be134bb2868728e879d2c08ab7244d
Alysson Zanchi <alyssonzanchi@gmail.com>
2024-08-28 17:54:02 -0300

    aula 147

---
+++ src/components/GridImage/mock.js
@@ -0,0 +1,38 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    'Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde',
+  grid: [
+    {
+      altText: 'Teste 1',
+      srcImg:
+        'https://images.unsplash.com/photo-1724182558344-f368493aa8b9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4Nzc4Mzl8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    {
+      altText: 'Teste 2',
+      srcImg:
+        'https://images.unsplash.com/photo-1723033868374-461843154f41?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4NzgwMTV8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    {
+      altText: 'Teste 3',
+      srcImg:
+        'https://images.unsplash.com/photo-1722842656953-402a77d54241?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4Nzc5NDV8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    {
+      altText: 'Teste 4',
+      srcImg:
+        'https://images.unsplash.com/photo-1723902499824-13f225769ebf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4NzgxMjZ8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    {
+      altText: 'Teste 5',
+      srcImg:
+        'https://images.unsplash.com/photo-1721846636945-9cbbb4584574?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4Nzc5Nzd8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    {
+      altText: 'Teste 6',
+      srcImg:
+        'https://images.unsplash.com/photo-1721989715402-2a50ed7b7f1f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDgwMzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjQ4NzgxMDh8&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+  ],
+};
